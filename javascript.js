70fa365cb0324c562dcb7589e78aaf41167025c5
Tombombleron <tombombleron@gmail.com>
2018-04-29 11:33:08 +0100

    Add timer functionality using JavaScript/jQuery

--- javascript.js
+++ javascript.js
@@ -16,18 +16,29 @@ $(document).ready(function() {
     const pauseButton = document.getElementById('pauseButton');
     const stopButton = document.getElementById('stopButton');
     
+    let inter;
+    let rounds = breakSetting.innerHTML;
+    
     sessionSetting.innerHTML = 25;
-    breakSetting.innerHTML =5;
+    breakSetting.innerHTML = 5;
     
     // timer settings adjustment functions
     sessionUp.onclick = function() {
-        sessionSetting.innerHTML++;
-        changeTimer();
+        if (sessionSetting.innerHTML < 59) {
+            sessionSetting.innerHTML++;
+            changeTimer();
+        } else {
+            window.alert('The timer cannot be set above 60 minutes!');
+        }
     };
     
     sessionDown.onclick = function() {
-        sessionSetting.innerHTML--;
-        changeTimer();
+        if (sessionSetting.innerHTML > 1) {
+            sessionSetting.innerHTML--;
+            changeTimer();
+        } else {
+            window.alert('The timer cannot be set to a negative number of minutes!');
+        }
     };
     
     function changeTimer() {
@@ -44,19 +55,64 @@ $(document).ready(function() {
     
     // timer manipulation functions
     startButton.onclick = function() {
-        let minutes = sessionSetting.innerHTML;
-        
+        disableStartButton();
+        let timeRemaining = sessionTimer.innerHTML.split(':');
+            let fiveMinutes = (timeRemaining[0] * 60) + parseInt(timeRemaining[1]),
+                display = sessionTimer;
+        startTimer(fiveMinutes, display);
     };
+    
     
     function resetTimer() {
-        
+        clearInterval(inter);
+        sessionTimer.innerHTML = sessionSetting.innerHTML + ':00';
+        startButton.style.pointerEvents = '';
     };
     
-    function pauseTimer() {
-        
+    resetButton.onclick = resetTimer;
+    stopButton.onclick = resetTimer;
+    
+    pauseButton.onclick = function () {
+        enableStartButton();
+        clearInterval(inter);
+    };
+
+    function startTimer(duration, display) {
+        var start = Date.now(),
+            diff,
+            minutes,
+            seconds;
+        function timer() {
+            // get the number of seconds that have elapsed since 
+            // startTimer() was called
+            diff = duration - (((Date.now() - start) / 1000) | 0);
+
+            // does the same job as parseInt truncates the float
+            minutes = (diff / 60) | 0;
+            seconds = (diff % 60) | 0;
+            
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds; 
+
+            if (diff <= 0) {
+                // add one second so that the count down starts at the full duration
+                // example 05:00 not 04:59
+                start = Date.now() + 1000;
+            }
+        };
+        // we don't want to wait a full second before the timer starts
+        timer();
+        inter = setInterval(timer, 1000);
+        }
+    
+    function disableStartButton() {
+        startButton.style.pointerEvents = 'none';
     };
     
-    function stopTimer() {
-        
+    function enableStartButton() {
+        startButton.style.pointerEvents = '';
     };
+    
 });
